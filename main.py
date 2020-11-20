78e2edc6110bdd4a33488b7221791ced7ca3ae76
Sharath Cherian Thomas <sharathct22@gmail.com>
2020-11-20 14:53:39 +0530

    flask server modified

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from flask import Flask
 import soundcloud
+import spotify_artist
+import spotify_playlist
+
 app = Flask(__name__)
 
 @app.route('/soundcloud/recent')
 def show_blog():
-   return soundcloud.get5()
+   return soundcloud.getrecent()
 
+@app.route('/soundcloud/popular')
+def show_blog():
+   return soundcloud.gethots()
+   
+@app.route('/spotify/artists/recent')
+def show_blog():
+   return spotify_artist.getrecent()
+   
+@app.route('/spotify/artists/popular')
+def show_blog():
+   return spotify_artist.gethots()
 
+@app.route('/spotify/playlist/recent')
+def show_blog():
+   return spotify_playlist.getrecent()
+   
+@app.route('/spotify/playlist/popular')
+def show_blog():
+   return spotify_playlist.gethots()
 
 if __name__ == '__main__':
    app.run()
